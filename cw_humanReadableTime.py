80b56efb18e332248f9ba2f1ca4aca1b7b3db484
Danny <danny@BOUINFMBP001.local>
2016-08-23 19:10:49 -0600

    added cw_humanReadbleTime.py

---
+++ cw_humanReadableTime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''https://www.codewars.com/kata/human-readable-time/train/python
+Human Readable Time
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+'''
+
+def make_readable(seconds):
+    
+
+
+
+Test.assert_equals(make_readable(0), "00:00:00")
+Test.assert_equals(make_readable(5), "00:00:05")
+Test.assert_equals(make_readable(60), "00:01:00")
+Test.assert_equals(make_readable(86399), "23:59:59")
+Test.assert_equals(make_readable(359999), "99:59:59")
