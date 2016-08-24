06e8fed21246d12d2db335a71229c35c0a86b135
Danny <danny@BOUINFMBP001.local>
2016-08-23 19:47:43 -0600

    finished cw_humanReadableTime.py

--- cw_humanReadableTime.py
+++ cw_humanReadableTime.py
@@ -14,12 +14,22 @@ You can find some examples in the test fixtures.
 '''
 
 def make_readable(seconds):
-    
+    hours = seconds // 60 // 60
+    minutes = seconds // 60 % 60
+    seconds = seconds % 60
+    if hours < 10:
+        hours = '0' + str(hours)
+    if minutes < 10:
+        minutes = '0' + str(minutes)
+    if seconds < 10:
+        seconds = '0' + str(seconds)
+    return str(hours) + ':' + str(minutes) + ':' + str(seconds)
+
+print(make_readable(3601))
 
 
-
-Test.assert_equals(make_readable(0), "00:00:00")
-Test.assert_equals(make_readable(5), "00:00:05")
-Test.assert_equals(make_readable(60), "00:01:00")
-Test.assert_equals(make_readable(86399), "23:59:59")
-Test.assert_equals(make_readable(359999), "99:59:59")
+#Test.assert_equals(make_readable(0), "00:00:00")
+#Test.assert_equals(make_readable(5), "00:00:05")
+#Test.assert_equals(make_readable(60), "00:01:00")
+#Test.assert_equals(make_readable(86399), "23:59:59")
+#Test.assert_equals(make_readable(359999), "99:59:59")
